3b86950b62e56c7547e820daad29c8aa9cb31df9
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-12 23:27:15 +0900

    started new project. Added the Hero section

---
+++ car2go/components/CustomButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {}}
+    >
+      <span className={"flex-1"}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
