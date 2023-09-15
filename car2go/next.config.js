9950bcd2d156974c9f887cff0d8543fc2301ef3e
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-16 01:41:39 +0900

    edited netlify.toml and added to the JSON file for netlify deployment

--- car2go/next.config.js
+++ car2go/next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    // other exports
-    target: 'serverless', // add this line
-  };
-
 module.exports = nextConfig
