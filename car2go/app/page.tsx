5cafb61a2d4d534775cffcf8e7987ba9ea0edc31
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-16 01:11:20 +0900

    netlify deployment modification

--- car2go/app/page.tsx
+++ car2go/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter />
+            <CustomFilter />
           </div>
         </div>
       </div>
