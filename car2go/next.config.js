ebe09005e5166506359832c6f307d5238ce0a4b5
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-16 02:31:32 +0900

    error fix netlify

--- car2go/next.config.js
+++ car2go/next.config.js
@@ -2,3 +2,17 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+// next.config.js
+
+module.exports = {
+  // Add the following configuration for static HTML export
+  target: 'experimental-serverless-trace', // Enables static HTML export
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      // Add more route mappings here if needed
+    };
+  },
+  // Other configuration options...
+};
