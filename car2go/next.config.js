c61ed7abe1ced73a989319cd2ee87a80393c64d8
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-16 01:32:14 +0900

    netlify error fix

--- car2go/next.config.js
+++ car2go/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+module.exports = {
+    // other exports
+    target: 'serverless', // add this line
+  };
+
 module.exports = nextConfig
